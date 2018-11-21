1e3cc561a59343f1d590c5c63001ab688136d355
Alexandra <alexswartz9753@gmail.com>
2018-11-21 17:00:55 -0500

    set up index.js file in react app

--- avengers/src/index.js
+++ avengers/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'; 
+
+import {avengersReducer} from './store/reducers';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reduxDevToolsHook = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(avengersReducer, compose(applyMiddleware(thunk, logger), reduxDevToolsHook));
+
+ReactDOM.render(<Provider store={store}>
+                    <Router>
+                        <App />
+                    </Router>
+                </Provider>, 
+                document.getElementById('root'));
+
