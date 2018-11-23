c49bb456a027edcd8dea442d712d53fb1d9e2cf8
Alexandra <alexswartz9753@gmail.com>
2018-11-23 13:01:25 -0500

    wrote reducer for all actions

--- avengers/src/index.js
+++ avengers/src/index.js
@@ -6,13 +6,13 @@ import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger'; 
 
-import {avengersReducer} from './store/reducers';
+import {reducer} from './store/reducers';
 import './index.css';
 import App from './App';
 
 const reduxDevToolsHook = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(avengersReducer, compose(applyMiddleware(thunk, logger), reduxDevToolsHook));
+const store = createStore(reducer, compose(applyMiddleware(thunk, logger), reduxDevToolsHook));
 
 ReactDOM.render(<Provider store={store}>
                     <Router>
